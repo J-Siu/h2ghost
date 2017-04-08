f64aab36d24cf995938b220068f5fb5b87d95fec
JS <js@johnsiu.com>
2017-04-08 05:14:53 -0400

    Improve comment. Add ghost.socketDelay.

--- h2ghost.config.js
+++ h2ghost.config.js
@@ -5,17 +5,18 @@ const
 /*--- Required Section ---*/
 
 const ghost = {
+
 	/* Ghost start mode:
-	'' - Do not start Ghost. h2ghost use proxy to access Ghost.
-	'app' - h2ghost will Start Ghost as backend server, but use Ghost's express rootApp directly.
-	'backend' - h2ghost will start Ghost as backend server, and use proxy to access it.
+		'' - Do not start Ghost. h2ghost use proxy to access Ghost.
+		'app' - h2ghost will Start Ghost as backend server, but use Ghost's express rootApp directly.
+		'backend' - h2ghost will start Ghost as backend server, and use proxy to access it.
 	*/
 	start: 'app',
 
 	/* Ghost start env, will also affect h2ghost.
-	- Override NODE_ENV.
-	- Override by command line option.
-	'production' | 'development' | 'testing'
+		- Override NODE_ENV.
+		- Override by command line option.
+		'production' | 'development' | 'testing'
 	*/
 	env: 'production',
 
@@ -29,22 +30,27 @@ const ghost = {
 	dir: '/home/ghost/ghost',
 	/* If `dir` empty, fill in 'url' and 'server' manually, otherwise can be left empty.
 
-	'url' -
-		This should have the same value the 'url' in Ghost's config.
+		'url' -
+			This should have the same value the 'url' in Ghost's config.
 
-	'server' -
-		Same format as in Ghost's config.js.
-		If h2ghost and Ghost are running in the same server,
-			this should be the same as 'server' in Ghost's config.
-		If Ghost is running in another server,
-			'server' should point to it accordingly.
+		'server' -
+			Same format as in Ghost's config.js.
+			If h2ghost and Ghost are running in the same server,
+				this should be the same as 'server' in Ghost's config.
+			If Ghost is running in another server,
+				'server' should point to it accordingly.
 	*/
 	url: '',
-	server: {}
+	server: {},
+
+	/* Delay front end startup if Ghost uses unix socket
+		Default: 20sec
+	*/
+	socketDelay: 20
 }
 
 /* cert
-This is the certificate object, same formate as https package.
+	This is the certificate object, same formate as https package.
 */
 const cert = {
 	key: fs.readFileSync('server.key'),
@@ -97,23 +103,23 @@ const helmetOptions = {
 	//referrerPolicy: {}, // https://helmetjs.github.io/docs/referrer-policy/
 
 	/* hsts - medium risk
-	This will lock your domain to HTTPS ONLY in client browser.
-	Make sure you understand throughly before enabling HSTS!!
-	Reference:
-		https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security
+		This will lock your domain to HTTPS ONLY in client browser.
+		Make sure you understand throughly before enabling HSTS!!
+		Reference:
+			https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security
 	*/
 	//hsts: {},
 
 	/* HPKP - HIGH RISK!!
-	If setup wrong,
-		THIS HAS THE POTENTIAL TO LOCK YOUR SITE/DOMAIN OUT OF CLIENT BROWSER FOR A LONG TIME!!
-	Make sure you understand throughly before enabling HPKP!!
-	If you do not understand HPKP, DON'T USE IT!!
-	HPKP Reference:
-		https://en.wikipedia.org/wiki/HTTP_Public_Key_Pinning
-		https://scotthelme.co.uk/hpkp-http-public-key-pinning/
-	Config Reference:
-		https://helmetjs.github.io/docs/hpkp/
+		If setup wrong,
+			THIS HAS THE POTENTIAL TO LOCK YOUR SITE/DOMAIN OUT OF CLIENT BROWSER FOR A LONG TIME!!
+		Make sure you understand throughly before enabling HPKP!!
+		If you do not understand HPKP, DON'T USE IT!!
+		HPKP Reference:
+			https://en.wikipedia.org/wiki/HTTP_Public_Key_Pinning
+			https://scotthelme.co.uk/hpkp-http-public-key-pinning/
+		Config Reference:
+			https://helmetjs.github.io/docs/hpkp/
 	*/
 	//hpkp: {}
 }
